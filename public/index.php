38f3628ad7e3075cf978ae226f62206c95b7bc8c
veerareddyobula <veerareddy.obula@gmail.com>
2019-07-27 08:37:59 +0530

    Binding React.js + Slim PHP In-Progress

--- public/index.php
+++ public/index.php
@@ -1,47 +1,33 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>APMC-Zonal-CME 2019</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  </head>
-  <body style="margin:0px !important; background-color: #E8EDF3; ">
-  <?php
-        if (PHP_SAPI == 'cli-server') {
-            // To help the built-in PHP dev server, check if the request was actually for
-            // something which should probably be served as a static file
-            $url  = parse_url($_SERVER['REQUEST_URI']);
-            $file = __DIR__ . $url['path'];
-            if (is_file($file)) {
-                return false;
-            }
-        }
+<?php
+  if (PHP_SAPI == 'cli-server') {
+      // To help the built-in PHP dev server, check if the request was actually for
+      // something which should probably be served as a static file
+      $url  = parse_url($_SERVER['REQUEST_URI']);
+      $file = __DIR__ . $url['path'];
+      if (is_file($file)) {
+          return false;
+      }
+  }
 
-        require __DIR__ . '/../vendor/autoload.php';
+  require __DIR__ . '/../vendor/autoload.php';
 
-        session_start();
+  session_start();
 
-        // Instantiate the app
-        $settings = require __DIR__ . '/../src/settings.php';
-        $app = new \Slim\App($settings);
+  // Instantiate the app
+  $settings = require __DIR__ . '/../src/settings.php';
+  $app = new \Slim\App($settings);
 
-        // Set up dependencies
-        $dependencies = require __DIR__ . '/../src/dependencies.php';
-        $dependencies($app);
+  // Set up dependencies
+  $dependencies = require __DIR__ . '/../src/dependencies.php';
+  $dependencies($app);
 
-        // Register middleware
-        $middleware = require __DIR__ . '/../src/middleware.php';
-        $middleware($app);
+  // Register middleware
+  $middleware = require __DIR__ . '/../src/middleware.php';
+  $middleware($app);
 
-        // Register routes
-        $routes = require __DIR__ . '/../src/routes.php';
-        $routes($app);
+  // Register routes
+  $routes = require __DIR__ . '/../src/routes.php';
+  $routes($app);
 
-        // Run app
-        $app->run();
-    ?>
-    <div id="root"></div>
-    <script src="./bundle/main.bundle.js"></script>
-  </body>
-</html>
+  // Run app
+  $app->run();

0a3bbfe3adaf220f0e97458e46d16da0735251b5
veerareddyobula <veerareddy.obula@gmail.com>
2019-07-27 08:29:28 +0530

    Integration with React.js is completed

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>(
+    <div>
+        Hello Veera
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
